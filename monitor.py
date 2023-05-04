f51060bd4b6e7adf917357f7331401c95e6f66b0
Maria-Boza <115089567+Maria-Boza@users.noreply.github.com>
2023-05-04 14:20:34 -0400

    fix method

--- monitor.py
+++ monitor.py
@@ -3,11 +3,8 @@ from plant_monitor import PlantMonitor
 
 monitor = PlantMonitor()
 
-def update_readings():
-    while True:
-        wetness.value = str(monitor.get_wetness())
-        temp.value = str(monitor.get_temp())
-        humidity.value = str(monitor.get_humidity())
-        time.sleep(2)
-
-update_readings()
+while True:
+    wetness.value = str(monitor.get_wetness())
+    temp.value = str(monitor.get_temp())
+    humidity.value = str(monitor.get_humidity())
+    time.sleep(2)
